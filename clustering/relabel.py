36f980db718ada8fc32a1b786999cb84f4b7ff54
Ormorod <Adam.Ormondroyd@gmail.com>
2022-09-13 12:41:55 +0100

    add relabel.py

---
+++ clustering/relabel.py
@@ -0,0 +1,16 @@
+i def relabel(labels):
+    """
+    Relabel cluster labels so that they are in ascending order.
+    """
+
+    appearance_order = {}
+    num_found = 0
+
+    for label in labels:
+        if label not in appearance_order:
+            appearance_order[label] = num_found
+            num_found += 1
+
+    for i, old_label in enumerate(labels):
+        labels[i] = appearance_order[old_label]
+    return labels
