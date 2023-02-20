d5921d9947cca7f5705759695f734883cfc6c8d0
Ormorod <Adam.Ormondroyd@gmail.com>
2023-02-20 13:41:32 +0000

    correctly calculate sigma2

--- clustering/bic.py
+++ clustering/bic.py
@@ -12,24 +12,24 @@ def bic(position_matrix, labels, cluster_centres):
     Li = np.full(K, np.inf)
     cluster_sizes = np.array([sum(labels == label)
                               for label in np.unique(labels)])
-    sqrt_sigma = 0
-
-    for i, pos in enumerate(position_matrix):
-        sqrt_sigma += np.sum(np.square(
-            np.array(pos) - np.array(cluster_centres[labels[i]])
-            ))
-
-    sqrt_sigma /= (R - K)
     p = (K - 1) + M * K + 1
 
-    if sqrt_sigma <= 0:
-        sigma_multiplier = -np.inf
-    else:
-        sigma_multiplier = M * 0.5 * np.log(sqrt_sigma)
+    for ii, Rn in enumerate(cluster_sizes):
+        sigma2 = 0
 
-    for i, Rn in enumerate(cluster_sizes):
+        for i, pos in enumerate(position_matrix):
+            sigma2 += np.sum(np.square(
+                np.array(pos) - np.array(cluster_centres[ii])
+                ))
 
-        Li[i] = (Rn * (np.log(Rn / R) - np.log(2 * np.pi) / 2
-                       - sigma_multiplier) - (Rn - K) / 2)
+        sigma2 /= (R - K)
+
+        if sigma2 <= 0:
+            sigma_multiplier = -np.inf
+        else:
+            sigma_multiplier = M * 0.5 * np.log(sigma2)
+
+        Li[ii] = (Rn * (np.log(Rn / R) - np.log(2 * np.pi) / 2
+                        - sigma_multiplier) - (Rn - K) / 2)
 
     return sum(Li) - p * np.log(R) / 2
