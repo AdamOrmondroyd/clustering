b8095b077e407dbe6bc4556a1a4c64258aac501f
Ormorod <Adam.Ormondroyd@gmail.com>
2023-03-17 19:25:28 +0000

    add knn to tests

--- tests/test_xmeans.py
+++ tests/test_xmeans.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from clustering import xmeans, sklearn_xmeans
+from clustering import xmeans, sklearn_xmeans, knn
 from clustering.bic import bic
 from pyclustering.utils import read_sample
 from pyclustering.samples.definitions import SIMPLE_SAMPLES
@@ -11,7 +11,7 @@ def position_matrix():
     return np.array(read_sample(SIMPLE_SAMPLES.SAMPLE_SIMPLE3))
 
 
-@pytest.mark.parametrize('algorithm', [xmeans, sklearn_xmeans])
+@pytest.mark.parametrize('algorithm', [xmeans, sklearn_xmeans, knn])
 def test_xmeans(algorithm, position_matrix):
     cluster_list = algorithm(position_matrix)
     assert max(cluster_list) + 1 == 4
