675a7b9bafc22b09c3804ea82fc7b2b9e8463fb8
Ormorod <Adam.Ormondroyd@gmail.com>
2023-03-17 16:18:29 +0000

    remove knn print statements

--- clustering/knn.py
+++ clustering/knn.py
@@ -30,12 +30,7 @@ def compute_knn(position_matrix, k):
     distance2_matrix = distance_matrix(position_matrix, position_matrix) ** 2
     knn_array = np.empty((npoints, k))
     for i in range(npoints):
-        try:
-            print(np.argsort(distance2_matrix[:, i]).shape)
-            knn_array[i] = np.argsort(distance2_matrix[:, i])[:k]  # [1:k+1]
-        except ValueError as e:
-            print(e)
-            raise e
+        knn_array[i] = np.argsort(distance2_matrix[:, i])[:k]  # [1:k+1]
 
     return knn_array
 
@@ -64,44 +59,31 @@ def knn(position_matrix):
     for n in np.arange(2, k + 1):
         labels = do_knn_clustering(knn_array[:, :n+1])
         num_clusters = max(labels) + 1
-        print(f"{num_clusters} clusters")
 
         if num_clusters <= 0:
             raise ValueError("somehow got <= 0 clusters")
         elif 1 == num_clusters:
-            print("no clusters found", flush=True)
             return labels
         elif np.all(labels == labels_old):
             break
         elif n == k:
             # If we need to cluster further, then expand the knn list
             k = min(k * 2, npoints)
-            print(k)
             # nn = NearestNeighbors(n_neighbors=k).fit(position_matrix)
             # knn_array = nn.kneighbors(position_matrix, return_distance=False)
             knn_array = compute_knn(position_matrix, k)
 
         labels_old = labels
 
-    print(f"{num_clusters} clusters")
     if num_clusters > 1:
         i_cluster = 0
         while i_cluster < num_clusters:
-            print(f"there are {num_clusters} clusters, with "
-                  f"sizes{[sum(labels == i) for i in range(num_clusters)]}")
-            print(f"gonna frikkin recurse {i_cluster}")
             cluster = knn_array[labels == i_cluster]
-            print(cluster.shape)
             labels[labels == i_cluster] = knn(cluster) + num_clusters
-            print(labels)
-            # labels = relabel(labels)
-            print(labels)
             # if didn't find any clusters, move on to the next one
             num_clusters_new = max(labels) + 1 - num_clusters
             if num_clusters_new == 1:
                 i_cluster += 1
             labels = relabel(labels)
             num_clusters = max(labels) + 1
-            print(labels, flush=True)
-    print("got to bottom of knn", flush=True)
     return labels
