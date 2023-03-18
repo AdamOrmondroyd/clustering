8780a2cfaa55943367f6c8108ffb64129675880a
Ormorod <Adam.Ormondroyd@gmail.com>
2023-03-18 14:56:26 +0000

    corrected knn algo

--- clustering/knn.py
+++ clustering/knn.py
@@ -3,7 +3,7 @@ Recursive KNN algorith copied from `PolyChord`
 """
 
 import numpy as np
-from sklearn.neighbors import NearestNeighbors
+from scipy.spatial import distance_matrix
 from clustering.relabel import relabel
 
 
@@ -24,9 +24,20 @@ def do_knn_clustering(knn_array):
     return relabel(labels)
 
 
+def compute_knn(position_matrix, k):
+    npoints = position_matrix.shape[0]
+    distance2_matrix = distance_matrix(position_matrix, position_matrix) ** 2
+    knn_array = np.empty((npoints, k))
+    for i in range(npoints):
+        knn_array[i] = np.argsort(distance2_matrix[:, i])[:k]  # [1:k+1]
+
+    return knn_array
+
+
 def knn(position_matrix):
     """
-    Returns cluster labels of position matrix using the K-Nearest-Neighbours algorithm.
+    Returns cluster labels of position matrix using the
+    K-Nearest-Neighbours algorithm.
 
     Slight concern if two points are the same because sklearn.
     """
@@ -35,16 +46,14 @@ def knn(position_matrix):
 
     npoints = position_matrix.shape[0]
     k = min(npoints, 10)
-    nn = NearestNeighbors(n_neighbors=k).fit(position_matrix)
-    knn_array = nn.kneighbors(position_matrix, return_distance=False)
+    knn_array = compute_knn(position_matrix, k)
     labels = np.arange(npoints)
     num_clusters = npoints
 
     labels_old = labels
-    num_clusters_old = num_clusters
 
-    for n in np.arange(2, k):
-        labels = do_knn_clustering(knn_array[:, :n])
+    for n in np.arange(2, k + 1):
+        labels = do_knn_clustering(knn_array[:, : n + 1])
         num_clusters = max(labels) + 1
 
         if num_clusters <= 0:
@@ -53,20 +62,22 @@ def knn(position_matrix):
             return labels
         elif np.all(labels == labels_old):
             break
-        elif k == n - 1:
+        elif n == k:
+            # If we need to cluster further, then expand the knn list
             k = min(k * 2, npoints)
-            nn = NearestNeighbors(n_neighbors=k).fit(position_matrix)
-            knn_array = nn.kneighbors(position_matrix, return_distance=False)
+            knn_array = compute_knn(position_matrix, k)
 
         labels_old = labels
-        num_clusters_old = num_clusters
 
     if num_clusters > 1:
         i_cluster = 0
         while i_cluster < num_clusters:
-            cluster = position_matrix[labels == i_cluster]
-            labels[labels == i_cluster] = do_knn_clustering(cluster) + num_clusters
-            labels = relabel(labels)
-            if num_clusters - 1 == max(labels):
+            cluster = knn_array[labels == i_cluster]
+            labels[labels == i_cluster] = knn(cluster) + num_clusters
+            # if didn't find any clusters, move on to the next one
+            num_clusters_new = max(labels) + 1 - num_clusters
+            if num_clusters_new == 1:
                 i_cluster += 1
+            labels = relabel(labels)
+            num_clusters = max(labels) + 1
     return labels
