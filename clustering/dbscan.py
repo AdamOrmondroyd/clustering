deb287587bf570c709eec8abc6be1d2945272dd2
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-07-13 14:25:16 +0100

    tidy print statements in DBSCAN and OPTICS

--- clustering/dbscan.py
+++ clustering/dbscan.py
@@ -2,18 +2,16 @@ from sklearn.cluster import DBSCAN, OPTICS
 
 
 def dbscan(position_matrix):
-    print("a")
+    print("DBSCAN clustering", flush=True)
     db = DBSCAN(eps=0.1).fit(position_matrix)
-    print(f"found {max(db.labels_)+1} clusters")
-    print("b")
-    print(db.labels_)
+    print(f"found {max(db.labels_)+1} clusters", flush=True)
+    print(db.labels_, flush=True)
     return db.labels_
 
 
 def optics(position_matrix):
-    print("a")
+    print("OPTICS clustering", flush=True)
     op = OPTICS().fit(position_matrix)
-    print(f"found {max(op.labels_)+1} clusters")
-    print("b")
-    print(op.labels_)
+    print(f"found {max(op.labels_)+1} clusters", flush=True)
+    print(op.labels_, flush=True)
     return op.labels_
