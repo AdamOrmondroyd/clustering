f40c437d5892a29e5ccfa2fea27f2f4be3f7cd5f
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-07-14 10:45:22 +0100

    refactor using fit_predict

--- clustering/dbscan.py
+++ clustering/dbscan.py
@@ -3,15 +3,17 @@ from sklearn.cluster import DBSCAN, OPTICS
 
 def dbscan(position_matrix):
     print("DBSCAN clustering", flush=True)
-    db = DBSCAN(eps=0.1).fit(position_matrix)
-    print(f"found {max(db.labels_)+1} clusters", flush=True)
-    print(db.labels_, flush=True)
-    return db.labels_
+    db = DBSCAN(eps=0.1)
+    labels = db.fit_predict(position_matrix)
+    print(f"found {max(labels)+1} clusters", flush=True)
+    print(labels, flush=True)
+    return labels
 
 
 def optics(position_matrix):
     print("OPTICS clustering", flush=True)
-    op = OPTICS().fit(position_matrix)
-    print(f"found {max(op.labels_)+1} clusters", flush=True)
-    print(op.labels_, flush=True)
-    return op.labels_
+    op = OPTICS()
+    labels = op.fit_predict(position_matrix)
+    print(f"found {max(labels)+1} clusters", flush=True)
+    print(labels, flush=True)
+    return labels
