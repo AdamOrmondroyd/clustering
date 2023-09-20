00643ddc8b130f7402f9c053e99d264babe3348f
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-09-20 15:23:51 +0100

    add HDBSCAN

--- clustering/dbscan.py
+++ clustering/dbscan.py
@@ -1,4 +1,4 @@
-from sklearn.cluster import DBSCAN, OPTICS
+from sklearn.cluster import DBSCAN, HDBSCAN, OPTICS
 from clustering.relabel import relabel
 
 
@@ -6,6 +6,15 @@ def dbscan(position_matrix):
     print("DBSCAN clustering", flush=True)
     db = DBSCAN(eps=0.1)
     labels = relabel(db.fit_predict(position_matrix))
+    print(f"found {max(labels)+1} clusters", flush=True)
+    print(labels, flush=True)
+    return labels
+
+
+def hdbscan(position_matrix):
+    print("HDBSCAN clustering", flush=True)
+    hdb = HDBSCAN(eps=0.1)
+    labels = relabel(hdb.fit_predict(position_matrix))
     print(f"found {max(labels)+1} clusters", flush=True)
     print(labels, flush=True)
     return labels
