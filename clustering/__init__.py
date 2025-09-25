6b6a6737f8695044c016517fa02f8609ef172bea
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-09-25 15:00:11 +0100

    add knn_recursive, to correctly emulate polychord behaviour

--- clustering/__init__.py
+++ clustering/__init__.py
@@ -4,6 +4,6 @@
 Keep using custom clustering algorithms everywhere so made sense to keep them together in this repo.
 """
 from clustering.xmeans import xmeans
-from clustering.knn import knn
+from clustering.knn import knn, knn_recursive
 from clustering.dbscan import dbscan, hdbscan, optics
 from clustering.jaxmeans import jaxmeans
